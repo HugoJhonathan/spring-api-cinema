9cf1fa0e5980c11eaacbb1ed511532a404e9f62b
FabianoFG <fabiano.f.gentil@gmail.com>
2022-08-11 20:46:00 -0300

    Primeiro commit

---
+++ cinema/src/test/java/br/com/notnullsistemas/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.notnullsistemas.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
