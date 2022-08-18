1759d20bc36bc1a2435daec91159f7e15cb5862b
cass-sep <lucas11mauricio@hotmail.com>
2022-08-17 21:00:01 -0300

    update criar bilhete

--- cinema/src/main/java/br/com/notnullsistemas/cinema/controller/BilheteController.java
+++ cinema/src/main/java/br/com/notnullsistemas/cinema/controller/BilheteController.java
@@ -1,9 +1,10 @@
 package br.com.notnullsistemas.cinema.controller;
 
-import br.com.notnullsistemas.cinema.core.crud.CrudController;
-import br.com.notnullsistemas.cinema.domain.Bilhete;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.notnullsistemas.cinema.core.crud.CrudController;
+import br.com.notnullsistemas.cinema.domain.Bilhete;
 
 @RestController
 @RequestMapping("/bilhetes")
--- cinema/src/main/java/br/com/notnullsistemas/cinema/core/crud/CrudController.java
+++ cinema/src/main/java/br/com/notnullsistemas/cinema/core/crud/CrudController.java
@@ -13,31 +13,31 @@ public abstract class CrudController<T extends CrudDomain<ID>, ID> {
     protected CrudService<T, ID> service;
 
     @GetMapping
-    public ResponseEntity<List<T>> todos(){
+    public ResponseEntity<List<T>> todos() {
         return ResponseEntity.ok(service.listar());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<T> listarUm(@PathVariable("id") ID id){
+    public ResponseEntity<T> listarUm(@PathVariable("id") ID id) {
         var entidade = service.porId(id);
         return ResponseEntity.ok(entidade);
     }
 
     @PostMapping
-    public ResponseEntity<T> criar(@RequestBody T entidade){
+    public ResponseEntity<T> criar(@RequestBody T entidade) {
         ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequest();
         var uri = builder.path("/{id}").buildAndExpand(entidade.getId()).toUri(); // id da entity criada
-        return ResponseEntity.created(uri).body(entidade);
+        return ResponseEntity.created(uri).body(service.criar(entidade));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<T> editar(@RequestBody T entidade, @PathVariable("id") ID id){
+    public ResponseEntity<T> editar(@RequestBody T entidade, @PathVariable("id") ID id) {
         var salvo = service.editar(id, entidade);
         return ResponseEntity.ok(salvo);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(@PathVariable("id") ID id){
+    public ResponseEntity<Void> deletar(@PathVariable("id") ID id) {
         service.excluir(id);
         return ResponseEntity.noContent().build();
     }
