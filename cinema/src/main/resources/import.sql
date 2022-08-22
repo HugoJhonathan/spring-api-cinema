de10ff0cb2abc3e3e7a310b2cc58ff7a44f4de29
cass-sep <lucas11mauricio@hotmail.com>
2022-08-21 21:17:47 -0300

    update import.sql

--- cinema/src/main/resources/import.sql
+++ cinema/src/main/resources/import.sql
@@ -15,13 +15,22 @@ INSERT INTO `pessoas`(`cpf`, `nome`) VALUES ('8023-184','Bruna Marques')
 
 INSERT INTO `filmes`(`atores`, `diretor`, `duracao`, `generos`, `nome`, `sinopse`) VALUES ('Tom Holland, Zendaya, Benedict Cumberbatch','Jon Watts',148,'super-herói','Homem-Aranha: Sem Volta para Casa','O Homem-Aranha precisa lidar com as consequências da sua verdadeira identidade ter sido descoberta.')
 
-INSERT INTO `filmes`(`atores`, `diretor`, `duracao`, `generos`, `nome`, `sinopse`) VALUES ('Timothée Chalamet, Zendaya, Rebecca Ferguson, Oscar Isaac, Josh Brolin','Denis Villeneuve',155,'ficção-científica','Duna','No futuro distante da humanidade, o duque Leto Atreides aceita a administração do perigoso planeta deserto Arrakis, a única fonte da substância mais valiosa do universo, a especiaria conhecida como "Melange", uma droga que prolonga a vida humana e fornece níveis acelerados de pensamento.')
+INSERT INTO `filmes`(`atores`, `diretor`, `duracao`, `generos`, `nome`, `sinopse`) VALUES ('Timothée Chalamet, Zendaya, Rebecca Ferguson, Oscar Isaac, Josh Brolin','Denis Villeneuve',155,'ficção-científica','Duna','No futuro distante da humanidade, o duque Leto Atreides aceita a administração do perigoso planeta deserto Arrakis, a única fonte da substância mais valiosa do universo.')
 
 INSERT INTO `filmes`(`atores`, `diretor`, `duracao`, `generos`, `nome`, `sinopse`) VALUES ('Alexander Skarsgård, Millie Bobby Brown, Rebecca Hall, Brian Tyree Henry','Adam Wingard',113,'ação, aventura','Godzilla vs. Kong','Kong e seus protetores embarcam em uma jornada perigosa para encontrar seu verdadeiro lar. No entanto, eles logo se encontram no caminho de Godzilla, completamente enfurecido, deixando um rastro de destruição em todo o mundo.')
 
 
-insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:12:00:00", 1, 1, 1);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:14:00:00", 1, 1, 1);
 insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:16:00:00", 1, 1, 1);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:20:00:00", 1, 1, 1);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:22:00:00", 1, 1, 1);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:12:00:00", 2, 5, 2);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:16:00:00", 2, 5, 2);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:20:00:00", 2, 5, 2);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:16:00:00", 3, 2, 2);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:18:00:00", 3, 2, 2);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:20:00:00", 3, 2, 2);
+insert into sessoes (horario, filme_id, sala_id, tipo_id) values ("2022-02-03:22:00:00", 3, 2, 2);
 
 insert into bilhetes (sessao_id, pessoa_id, meia, poltrona) values (1, 1, true, 1);
 insert into bilhetes (sessao_id, pessoa_id, meia, poltrona) values (1, 2, false, 2);
