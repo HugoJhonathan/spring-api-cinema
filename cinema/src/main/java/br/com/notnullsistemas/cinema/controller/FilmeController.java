a21f4d534a8c968b757df90586e024a720dfdd8e
cass-sep <lucas11mauricio@hotmail.com>
2022-08-21 21:07:38 -0300

    query filmes/sessoes
    mostra as todas as sessoes transmitindo um filme

--- cinema/src/main/java/br/com/notnullsistemas/cinema/controller/FilmeController.java
+++ cinema/src/main/java/br/com/notnullsistemas/cinema/controller/FilmeController.java
@@ -2,12 +2,31 @@ package br.com.notnullsistemas.cinema.controller;
 
 import br.com.notnullsistemas.cinema.core.crud.CrudController;
 import br.com.notnullsistemas.cinema.domain.Filme;
+import br.com.notnullsistemas.cinema.domain.Sessao;
 import br.com.notnullsistemas.cinema.dto.FilmeDTO;
+import br.com.notnullsistemas.cinema.repository.FilmeRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/filmes")
 public class FilmeController extends CrudController<Filme, FilmeDTO, Long> {
-    
+
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+    @GetMapping("/{id}/sessoes")
+    public ResponseEntity<List<Sessao>> listarSessoes(@PathVariable("id") Long id) {
+
+        List<Sessao> listarSessoes = filmeRepository.sessoesPorFilme(id);
+
+        return ResponseEntity.ok(listarSessoes);
+    }
 }
--- cinema/src/main/java/br/com/notnullsistemas/cinema/repository/FilmeRepository.java
+++ cinema/src/main/java/br/com/notnullsistemas/cinema/repository/FilmeRepository.java
@@ -2,9 +2,17 @@ package br.com.notnullsistemas.cinema.repository;
 
 import br.com.notnullsistemas.cinema.core.crud.CrudRepository;
 import br.com.notnullsistemas.cinema.domain.Filme;
+import br.com.notnullsistemas.cinema.domain.Sessao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FilmeRepository extends CrudRepository<Filme, Long> {
 
+    @Query(value = "FROM Sessao s JOIN s.filme f WHERE f.id = :id")
+    List<Sessao> sessoesPorFilme(@Param("id") Long id);
 }
