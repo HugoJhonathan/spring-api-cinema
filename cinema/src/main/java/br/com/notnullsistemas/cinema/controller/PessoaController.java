accc699908e8f7b188651d2e6c6f2982a4ea80b9
hugo_ <40052440+HugoJhonathan@users.noreply.github.com>
2022-08-22 13:18:32 -0300

    getBilhetesDeUmaPessoa

--- cinema/src/main/java/br/com/notnullsistemas/cinema/controller/PessoaController.java
+++ cinema/src/main/java/br/com/notnullsistemas/cinema/controller/PessoaController.java
@@ -1,13 +1,40 @@
 package br.com.notnullsistemas.cinema.controller;
 
+import br.com.notnullsistemas.cinema.converter.BilheteConverter;
 import br.com.notnullsistemas.cinema.core.crud.CrudController;
+import br.com.notnullsistemas.cinema.domain.Bilhete;
 import br.com.notnullsistemas.cinema.domain.Pessoa;
+import br.com.notnullsistemas.cinema.dto.BilheteDTO;
 import br.com.notnullsistemas.cinema.dto.PessoaDTO;
+import br.com.notnullsistemas.cinema.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController extends CrudController<Pessoa, PessoaDTO, Long> {
-    
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private BilheteConverter bilheteConverter;
+
+    @GetMapping("/{id}/bilhetes")
+    public ResponseEntity<List<BilheteDTO>> getBilhetesDeUmaPessoa(@PathVariable("id") Long id){
+        List<Bilhete> bilhetes = pessoaRepository.bilhetesDeUmaPessoa(id);
+        List<BilheteDTO> bilhetesDTO = bilhetes.stream()
+                .map(bilheteConverter::entidadeParaDto)
+                .collect(Collectors.toList());
+
+       return ResponseEntity.ok(bilhetesDTO);
+    }
+
 }
--- cinema/src/main/java/br/com/notnullsistemas/cinema/repository/PessoaRepository.java
+++ cinema/src/main/java/br/com/notnullsistemas/cinema/repository/PessoaRepository.java
@@ -1,10 +1,17 @@
 package br.com.notnullsistemas.cinema.repository;
 
 import br.com.notnullsistemas.cinema.core.crud.CrudRepository;
+import br.com.notnullsistemas.cinema.domain.Bilhete;
 import br.com.notnullsistemas.cinema.domain.Pessoa;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
 
+    @Query("from Bilhete b join b.pessoa p where p.id=:id")
+    List<Bilhete> bilhetesDeUmaPessoa(@Param("id") Long id);
 }
